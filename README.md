012e02fc3159d342ef426b6fbd106c341a3dea80
Amit Kumar <toamitkumar@gmail.com>
2011-05-14 12:18:33 -0400

    Added javascript extension and better details

---
+++ README.md
@@ -0,0 +1,7 @@
+Creating Waterfall Charts using [highcharts](http://www.highcharts.com/) JS library.
+
+The extension takes care of connecting the bar edges.
+
+![Waterfall Chart](http://2.bp.blogspot.com/-w_hamR6mawo/Tc6q2eiTbBI/AAAAAAAAEQM/9QCE2sKqJwY/s320/watrefall.png)
+
+Look at the examples directory for details.
---
+++ javascripts/mylib.js
@@ -0,0 +1,67 @@
+$.fn.waterfallChart = function(options, yaxis_max) {
+	var element = $(this),
+			chartOptions = {},
+			barCount = options.series.data.length,
+			data = options.series.data,
+			seriesOptions = [];
+
+	seriesOptions.push({
+		type: 'column',
+		cursor: 'pointer',
+		point: {
+			events: {
+				click: function() { 
+					alert(this.series.name+" --- "+this.category);
+				}
+			}
+		},
+    name: options.series.name,
+		borderColor: 'black',
+    data: options.series.data,
+		groupPadding: 0,
+		pointPadding:.1,
+	});
+	
+	// loop through all except the last one - last one is Total
+	for (var i=0; i < barCount-1; i++) {
+		seriesOptions.push($.lineConnector([[i, data[i].low], [i+1, data[i].low]]));
+	};
+			
+	chartOptions = {
+		chart: {
+			renderTo: element.attr("id")
+		},
+		title: {text: ''},
+		legend: {enabled: false},
+		tooltip: { enabled: false},
+		credits: {enabled: false},
+		yAxis: {
+			title: {text: null},
+			min: 0,
+			max: options.ymax,
+			gridLineColor: null,
+			labels: {enabled: false}
+		},
+		xAxis: {
+			categories: options.categories,
+			lineWidth: 2,
+			lineColor: 'black'
+		},	
+		plotOptions: {allowPointSelect: true},
+	  series: seriesOptions
+	}
+	return new Highcharts.Chart(chartOptions);
+};
+
+$.lineConnector = function(lineData) {
+	var lineConnectorOptions = {
+		data: [],
+		color: 'black',
+		dashStyle: 'dash',
+		marker: {enabled: false},
+		lineWidth: 1,
+		shadow: false,
+		enableMouseTracking: false
+	}
+	return $.extend(lineConnectorOptions, {data: lineData});
+};
